da119f01005ab2e4d7e27caff09ede35158a27fd
sinofseven <em.s.00001@gmail.com>
2019-12-03 21:40:05 +0900

    add tweet function

---
+++ src/tweet/index.py
@@ -0,0 +1,14 @@
+from logger.my_logger import MyLogger
+from client import main
+from tools.aws_tools import save_lambda_request_id
+
+logger = MyLogger(__name__)
+
+
+@save_lambda_request_id()
+def handler(event, context):
+    try:
+        main(event)
+    except Exception as e:
+        logger.error(f"Exception occurred: {e}")
+        raise
